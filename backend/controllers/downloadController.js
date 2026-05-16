372c1d4daea79ae697401d5b92ba8149b3ed49da
designeruiux <designeruiux253@gmail.com>
2026-05-16 09:45:23 +0530

    fixed again

--- backend/controllers/downloadController.js
+++ backend/controllers/downloadController.js
@@ -1,4 +1,5 @@
-import puppeteer from "puppeteer";
+import puppeteer from "puppeteer-core";
+import chromium from "@sparticuz/chromium";
 
 export const homeController = (req, res) => {
   res.send("Pinterest Downloader API Running...");
@@ -19,12 +20,11 @@ export const downloadController = async (req, res) => {
       });
     }
 
- browser = await puppeteer.launch({
-  headless: "new",
-  args: [
-    "--no-sandbox",
-    "--disable-setuid-sandbox",
-  ],
+browser = await puppeteer.launch({
+  args: chromium.args,
+  defaultViewport: chromium.defaultViewport,
+  executablePath: await chromium.executablePath(),
+  headless: true,
 });
 
 const page = await browser.newPage();

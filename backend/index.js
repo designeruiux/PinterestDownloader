da1f23409cc9b4d5dedb3e146bcb5e8f3af1304c
designeruiux <designeruiux253@gmail.com>
2026-05-19 13:43:42 +0530

    add code express

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,13 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import path from "path";
 import downloadRoutes from "./routes/downloadRoutes.js";
 
 dotenv.config();
 
 const app = express();
+const __dirname = path.resolve();
 
 app.use(cors({
   origin: [
@@ -16,10 +18,16 @@ app.use(cors({
 
 app.use("/", downloadRoutes);
 
+// 👉 serve frontend
+app.use(express.static(path.join(__dirname, "dist")));
+
+// 👉 SPA fallback
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "dist", "index.html"));
+});
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log(
-    `Server running on http://localhost:${PORT}`
-  );
+  console.log(`Server running on http://localhost:${PORT}`);
 });
